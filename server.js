f03e3c0576dcfafbb88761aa2ffe24ce355ca9ec
mjimenez05 <matthewjjimenez05@hotmail.com>
2020-06-02 11:37:00 -0600

    front end working with the survey and home page, need to render json still

---
+++ server.js
@@ -0,0 +1,46 @@
+const htmlRoutes = require("./app/routing/htmlRoutes");
+const apiRoutes = require("./app/routing/apiRoutes");
+const friends = require("./app/data/friends");
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3306;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, './app/public')))
+
+htmlRoutes(app, path);
+apiRoutes(app);
+
+module.exports.lookForFriends = function () {
+    let scoreArray = [];
+    let current = apiRoutes.user;
+
+    for (let i = 0; i < friends.array.length; i++) {
+        let friendScore = 0;
+        if (friends.array[i].name !== current.name) {
+
+            for (let j = 0; j < friends.array[i].scores.length; j++) {
+                let score = friends.array[i].scores[j];
+                let difference = math.abs(score - current.scores[j])
+                friendScore+=difference;
+            }
+            let friendObj = {};
+            friendObj.friend = friends.array[i];
+            friendObj.score = friendScore;
+            scoreArray.push(friendObj)
+        }
+    }
+    scoreArray.sort(function (a,b){
+        return a.score - b.score;
+    });
+    return scoreArray[0]
+}
+app.listen(PORT, function(){
+    console.log("App listening on http://localhost:" + PORT);
+});
